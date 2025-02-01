3f721ab78acf9950ff0edf74a208e7ba114a7939
FRC8889 <FRC8889@gmail.com>
2025-02-01 14:17:24 -0500

    New Sonsor Code - to detect coral

--- src/main/java/frc/robot/subsystems/RandomNeo.java
+++ src/main/java/frc/robot/subsystems/RandomNeo.java
@@ -4,14 +4,15 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class RandomNeo extends SubsystemBase{
 
     private final SparkMax tinyNeo = new SparkMax(9, MotorType.kBrushless);
-
+    private final DigitalInput sensor = new DigitalInput(0);
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Neo encoder", getEncoder());
+        SmartDashboard.putBoolean("Neo encoder", sensor.get() );
 
     }
 
